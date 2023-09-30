4fca55e6fbcfa3f9768517480696764b5c88e83e
AzureAD\DANIELPEREZGONZALEZ <daniel.perez@enti.cat>
2023-09-30 13:48:50 +0200

    Persigue el enemigo

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -1,29 +1,16 @@
 extends CharacterBody3D
 
+class_name Enemy
 
 const SPEED = 5.0
-var player
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
-func _ready():
-	player = get_node("/root/Escenario/CharacterBody3D")
-	
-	pass
+@onready var target = $"../Character"
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	print(player.transform)
-	var input_dirX = player.transform.basis.x - transform.basis.x
-	var input_dirZ = player.transform.basis.z - transform.basis.z
-	var direction = (Vector3(input_dirX.x, 0, input_dirZ.z)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
-	move_and_slide()
+	print(target.transform)
+	if target == null: get_tree().get_nodes_in_group("Character")
+	if target != null:
+		velocity = position.direction_to(target.position) * SPEED
+		move_and_slide()
