f2f012f05e4ca297f692dd796dc5e67d4fe2db5e
Alejandro Lopez Huesca <alejandro.lopez@enti.cat>
2023-09-30 14:31:31 +0200

    Rotación del personaje

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 class_name Character
 
 const SPEED = 5.0
+const ANGULAR_SPEED = 0.02
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -16,7 +17,8 @@ func _process(delta):
 	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	# Mouse movement.
-	
+	_mouse_motion = Input.get_last_mouse_velocity()
+	rotate_y(_mouse_motion.x * ANGULAR_SPEED * delta)
 	
 	if Input.is_action_just_pressed("attack"):
 		print("Ataca perra")
