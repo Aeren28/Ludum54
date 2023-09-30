86ebcdca3ac9b99a833ea97b1ad64c28774eb15b
Alejandro Lopez Huesca <alejandro.lopez@enti.cat>
2023-09-30 12:12:40 +0200

    Pasando el input al _process()

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -7,6 +7,13 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var input_dir
+var direction
+
+func _process(delta):
+	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -15,8 +22,6 @@ func _physics_process(delta):
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
