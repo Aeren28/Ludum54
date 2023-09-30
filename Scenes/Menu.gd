63d7b7350917bc7fd6d1092556e3d0225e5ab0f0
Tom Renkema <tomrenkema@live.nl>
2023-10-01 01:22:19 +0200

    Main Menu

---
+++ Scenes/Menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"MarginContainer/VBoxContainer2/MarginContainer/VBoxContainer/Start Button Container/Start Button".grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Escenario.tscn")
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
