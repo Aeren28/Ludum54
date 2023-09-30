ed5e9af54e7dea920c5ceff8c294a75b58d54b3d
AzureAD\DANIELPEREZGONZALEZ <daniel.perez@enti.cat>
2023-09-30 18:07:30 +0200

    Enemy spawner listo

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,15 +2,13 @@ extends CharacterBody3D
 
 class_name Enemy
 
-const SPEED = 5.0
+var SPEED = randf_range(1, 4)
 
-@onready var target = $"../Character"
+@onready var target = get_tree().get_root().get_node("Escenario").get_node("Character")
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	print(target.transform)
-	if target == null: get_tree().get_nodes_in_group("Character")
 	if target != null:
 		velocity = position.direction_to(target.position) * SPEED
 		move_and_slide()
