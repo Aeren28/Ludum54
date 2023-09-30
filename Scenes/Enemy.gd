a8ff41605a90bfcdccd8547e8563ec72b402d9d1
Alejandro Lopez Huesca <alejandro.lopez@enti.cat>
2023-09-30 17:52:18 +0200

    Rehaciendo el proyectil

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -9,7 +9,6 @@ const SPEED = 5.0
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	print(target.transform)
 	if target == null: get_tree().get_nodes_in_group("Character")
 	if target != null:
 		velocity = position.direction_to(target.position) * SPEED
