2042795683af7f89d5e40545edc43112d59aa6f5
Tom Renkema <tomrenkema@live.nl>
2023-10-01 16:37:48 +0200

    Health Bar working

--- Scenes/GameplayUI.gd
+++ Scenes/GameplayUI.gd
@@ -2,15 +2,15 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer2/Score.text = "0"
-
+	$MarginContainer/VBoxContainer/Score.text = "0"
+	$MarginContainer/VBoxContainer/HealthBar.value = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func setScore(score):
-	$MarginContainer2/Score.set_text(str(score))
+	$MarginContainer/VBoxContainer/Score.set_text(str(score))
 
 func setHealthbar(percentage):
-	pass
+	$MarginContainer/VBoxContainer/HealthBar.value = percentage - 9
